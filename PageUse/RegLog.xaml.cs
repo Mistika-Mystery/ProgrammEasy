97df785aeec3b6847d7e03295f85afb2485021d9
Мирослава Сергеева <m-bazhenova@yandex.ru>
2024-04-30 17:12:49 +0300

    ewf

--- PageUse/RegLog.xaml.cs
+++ PageUse/RegLog.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProgrammEasy.WinUse;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -27,7 +28,8 @@ namespace ProgrammEasy.PageUse
 
         private void LogInfoBT_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-
+            var infoLog = new InfoLogin();
+            infoLog.Show();
         }
 
         private void NextBT_Click(object sender, RoutedEventArgs e)
