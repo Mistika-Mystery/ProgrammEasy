7b726586a679c611afb75e7394059c37b1e4aa26
Мирослава Сергеева <m-bazhenova@yandex.ru>
2024-05-03 10:12:53 +0300

    asv

--- PageUse/RegLastName.xaml.cs
+++ PageUse/RegLastName.xaml.cs
@@ -70,16 +70,20 @@ namespace ProgrammEasy.PageUse
                     // Удаляем два и более подряд идущих дефиса, оставляя только один, если за ним есть буква
                     text = System.Text.RegularExpressions.Regex.Replace(text, @"-{2,}", "-");
                     text = System.Text.RegularExpressions.Regex.Replace(text, @"-(?![а-яА-ЯёЁ])", "");
+                                       
+                    // Делаем буквы после дефиса заглавными
+                    text = System.Text.RegularExpressions.Regex.Replace(text, @"-(\p{L})", match => "-" + match.Groups[1].Value.ToUpper());
 
+                    // Удаляем начальный дефис, если он есть
+                    if (text.StartsWith("-"))
+                    {
+                        text = text.Substring(1);
+                    }
                     // Если первая буква не заглавная, делаем ее заглавной
                     if (!string.IsNullOrEmpty(text) && char.IsLower(text[0]))
                     {
                         text = char.ToUpper(text[0]) + text.Substring(1).ToLower();
                     }
-
-                    // Делаем буквы после дефиса заглавными
-                    text = System.Text.RegularExpressions.Regex.Replace(text, @"-(\p{L})", match => "-" + match.Groups[1].Value.ToUpper());
-
                     textBox.Text = text;
                 }
             }
