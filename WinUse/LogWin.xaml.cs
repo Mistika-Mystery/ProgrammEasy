e4f41a5fc0927f2cac61da2d04fa76630477c408
Мирослава Сергеева <m-bazhenova@yandex.ru>
2024-05-13 10:36:36 +0300

    в

--- WinUse/LogWin.xaml.cs
+++ WinUse/LogWin.xaml.cs
@@ -41,6 +41,7 @@ namespace ProgrammEasy.WinUse
                     RegFlag.IdUser = UserLog.Id;
                     RegFlag.UserName = UserLog.FirstName;
                     RegFlag.UserLastName=UserLog.LastName;
+                    //RegFlag.RoleName=UserLog.Role.Name;
 
                     var mainWin = new MainWinUse();
                     mainWin.Show();
