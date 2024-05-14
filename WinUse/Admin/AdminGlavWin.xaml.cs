64b9d9aaea29170877a657933eb9e3b6778a3a1c
Мирослава Сергеева <m-bazhenova@yandex.ru>
2024-05-14 14:24:09 +0300

    poisk

--- WinUse/Admin/AdminGlavWin.xaml.cs
+++ WinUse/Admin/AdminGlavWin.xaml.cs
@@ -22,7 +22,7 @@ namespace ProgrammEasy.WinUse.Admin
         public AdminGlavWin()
         {
             InitializeComponent();
-            ReqDG.ItemsSource = my01Entities.GetContext().Requests.ToList();
+        
         }
 
         private void ExitBT_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -42,6 +42,93 @@ namespace ProgrammEasy.WinUse.Admin
         private void DataGridRow_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
 
+        }
+
+        private void AddBT_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DelBT_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SeactWater_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(SeactWater.Text))
+            {
+                SeactWater.Visibility = Visibility.Collapsed;
+                TBoxSearch.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void SeactWater_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Seach_Filter(SeactWater.Text);
+        }
+
+        private void TBoxSearch_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TBoxSearch.Visibility = Visibility.Collapsed;
+            SeactWater.Visibility = Visibility.Visible;
+            SeactWater.Focus();
+        }
+
+        private void sortBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //Seach_Filter_Films(SeactWater.Text);
+        }
+
+        private void CBTip_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //Seach_Filter_Films(SeactWater.Text);
+        }
+
+        private void CBJanr_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //Seach_Filter_Films(SeactWater.Text);
+        }
+
+        private void CBStrana_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //Seach_Filter_Films(SeactWater.Text);
+        }
+
+        private void BtnReload_Click(object sender, RoutedEventArgs e)
+        {
+            UpdWin();
+        }
+        private void Seach_Filter(string search = "")
+        {
+            var ReqSerch = my01Entities.GetContext().Requests.ToList();
+
+            if (!string.IsNullOrEmpty(search) || !string.IsNullOrWhiteSpace(search))
+            {
+                //по наименованию и описанию 
+
+                ReqSerch = ReqSerch.Where(s => s.User.Login.ToLower().Contains(search.ToLower())
+                || (s.Description ?? "").ToLower().Contains(search.ToLower())).ToList();
+            }
+            ReqDG.ItemsSource = ReqSerch;
+        }
+
+        private void AdminWin_Loaded(object sender, RoutedEventArgs e)
+        {
+            UpdWin();
+        }
+
+        private void AdminWin_Activated(object sender, EventArgs e)
+        {
+            UpdWin();
+        }
+        private void UpdWin()
+        {
+            SeactWater.Visibility = Visibility.Collapsed;
+            SeactWater.Text = "";
+            TBoxSearch.Visibility = Visibility.Visible;
+
+            ReqDG.ItemsSource = my01Entities.GetContext().Requests.ToList();
         }
     }
 }
