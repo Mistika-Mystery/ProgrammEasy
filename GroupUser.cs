4afebf87f7ca2db56df958aa9f397b60345ff7ff
Мирослава Сергеева <m-bazhenova@yandex.ru>
2024-05-27 23:42:59 +0300

    вв

--- GroupUser.cs
+++ GroupUser.cs
@@ -22,11 +22,9 @@ namespace ProgrammEasy
     
         public int Id { get; set; }
         public string Name { get; set; }
-        public Nullable<int> IdTeacher { get; set; }
         public byte[] Img { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<User> User { get; set; }
-        public virtual User User1 { get; set; }
     }
 }
