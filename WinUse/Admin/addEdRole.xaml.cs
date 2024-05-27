bdbab62d6a2a56d6ee07cdf6215a48efa96fb084
Мирослава Сергеева <m-bazhenova@yandex.ru>
2024-05-27 17:02:03 +0300

    cd

--- WinUse/Admin/addEdRole.xaml.cs
+++ WinUse/Admin/addEdRole.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.NetworkInformation;
@@ -24,6 +25,7 @@ namespace ProgrammEasy.WinUse.Admin
         private RoleUser _role = new RoleUser();
         Regex nazvania = new Regex(@"^[А-ЯЁ][а-яё\s]{2,50}$");
         MatchCollection match;
+        private byte[] data = null;
         public addEdRole(RoleUser role)
         {
             InitializeComponent();
@@ -34,6 +36,7 @@ namespace ProgrammEasy.WinUse.Admin
             DataContext = _role;
             lable.Content = _role.Name;
             lable.Visibility = Visibility.Hidden;
+            if (_role.Img != null) ImageSerice.Source = new ImageSourceConverter().ConvertFrom(_role.Img) as ImageSource;
         }
 
         private void BackBTN_Click(object sender, RoutedEventArgs e)
@@ -74,6 +77,7 @@ namespace ProgrammEasy.WinUse.Admin
                     }
                     else
                     {
+                        _role.Img = data;
                         myEntities.GetContext().RoleUser.Add(_role);
                         myEntities.GetContext().SaveChanges();
                         MessageBox.Show("Роль успешно добавлена!");
@@ -99,6 +103,26 @@ namespace ProgrammEasy.WinUse.Admin
                         MessageBox.Show("Изменения сохранены!");
                         this.Close();
                     }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void SelectImageBTN_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                OpenFileDialog fileOpen = new OpenFileDialog();
+                fileOpen.Multiselect = false;
+                fileOpen.Filter = "Image | *.png; *.jpg; *.jpeg";
+                if (fileOpen.ShowDialog() == true)
+                {
+                    data = System.IO.File.ReadAllBytes(fileOpen.FileName);
+
+                    ImageSerice.Source = new ImageSourceConverter().ConvertFrom(data) as ImageSource;
                 }
             }
             catch (Exception ex)
