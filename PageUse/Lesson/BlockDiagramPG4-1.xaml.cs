e2a3be8b9e61f20c9bbc46948c46c98ed9810a44
Мирослава Сергеева <m-bazhenova@yandex.ru>
2024-05-30 14:50:02 +0300

    sdv

--- PageUse/Lesson/BlockDiagramPG4-1.xaml.cs
+++ PageUse/Lesson/BlockDiagramPG4-1.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -10,15 +11,30 @@ namespace ProgrammEasy.PageUse.Lesson
         private bool isDragging = false;
         private Point clickPosition;
         private TranslateTransform currentTransform;
+        private Point[] initialPositions; ///////
 
         public BlockDiagramPG4_1()
         {
             InitializeComponent();
+            SaveInitialPositions();
+
+        }
+        private void SaveInitialPositions()
+        {
+            img01.Tag = new Point(Grid.GetColumn(img01), Grid.GetRow(img01));
+            img02.Tag = new Point(Grid.GetColumn(img02), Grid.GetRow(img02));
+            img03.Tag = new Point(Grid.GetColumn(img03), Grid.GetRow(img03));
+            img04.Tag = new Point(Grid.GetColumn(img04), Grid.GetRow(img04));
+            img05.Tag = new Point(Grid.GetColumn(img05), Grid.GetRow(img05));
+            img06.Tag = new Point(Grid.GetColumn(img06), Grid.GetRow(img06));
+            img07.Tag = new Point(Grid.GetColumn(img07), Grid.GetRow(img07));
+            img08.Tag = new Point(Grid.GetColumn(img08), Grid.GetRow(img08));
         }
 
         private void BakcBT_Click(object sender, RoutedEventArgs e)
         {
-            // Навигация назад
+            NavigationService.Navigate(new BlockDiagramPG5());
+
         }
 
         private void NextBT_Click(object sender, RoutedEventArgs e)
@@ -40,7 +56,10 @@ namespace ProgrammEasy.PageUse.Lesson
             isDragging = false;
             Image image = sender as Image;
             image.ReleaseMouseCapture();
-            CheckIfAllTargetsCorrect();
+
+            // Сохраняем текущие позиции изображения в Tag
+            Point currentPosition = new Point(Grid.GetColumn(image), Grid.GetRow(image));
+            image.Tag = currentPosition;
         }
 
         private void Image_MouseMove(object sender, MouseEventArgs e)
@@ -59,16 +78,17 @@ namespace ProgrammEasy.PageUse.Lesson
             }
         }
 
-        private void CheckIfAllTargetsCorrect()
+        private bool CheckIfAllTargetsCorrect()
         {
             // Логика проверки
             bool isCorrect = IsInTarget(img01, Target1) &&
                              IsInTarget(img02, Target2) &&
                              IsInTarget(img03, Target3) &&
-                             IsInTarget(img05, Target4) &&
+                             IsInTarget(img04, Target4) &&
                              IsInTarget(img05, Target5);
+            return isCorrect;
 
-            NextBT.IsEnabled = isCorrect;
+            
         }
 
         private bool IsInTarget(Image img, Border target)
@@ -82,6 +102,64 @@ namespace ProgrammEasy.PageUse.Lesson
                                                 targetBounds.Width + 2 * margin, targetBounds.Height + 2 * margin);
 
             return imgBounds.IntersectsWith(extendedTargetBounds);
+        }
+
+        private void CheckBT_Click(object sender, RoutedEventArgs e)
+        {
+            if (CheckBT.Content.ToString() == "Проверить")
+            {
+
+                if (CheckIfAllTargetsCorrect())
+                {
+                    // Пользователь расставил картинки верно
+                    CheckBT.Content = "Молодец!";
+                    NextBT.IsEnabled = true;
+                }
+                else
+                {
+                    // Пользователь расставил картинки неверно
+                    //ResetImages();
+                    CheckBT.Content = "Попробовать еще раз";
+                }
+            }
+            else if (CheckBT.Content.ToString() == "Попробовать еще раз")
+            {
+                
+                ResetImages();
+            }
+
+        }
+        private void ResetImages()
+        {
+            Console.WriteLine("Resetting images...");
+            Console.WriteLine($"Initial position of img01: {img01.Tag}");
+            Console.WriteLine($"Initial position of img02: {img02.Tag}");
+            Console.WriteLine($"Initial position of img03: {img03.Tag}");
+            Console.WriteLine($"Initial position of img04: {img04.Tag}");
+            Console.WriteLine($"Initial position of img05: {img05.Tag}");
+            Console.WriteLine($"Initial position of img06: {img06.Tag}");
+            Console.WriteLine($"Initial position of img07: {img07.Tag}");
+            Console.WriteLine($"Initial position of img08: {img08.Tag}");
+
+            // Возвращаем изображения на исходные позиции в разметке грида
+            SetImagePosition(img01, (Point)img01.Tag);
+            SetImagePosition(img02, (Point)img02.Tag);
+            SetImagePosition(img03, (Point)img03.Tag);
+            SetImagePosition(img04, (Point)img04.Tag);
+            SetImagePosition(img05, (Point)img05.Tag);
+            SetImagePosition(img06, (Point)img06.Tag);
+            SetImagePosition(img07, (Point)img07.Tag);
+            SetImagePosition(img08, (Point)img08.Tag);
+
+            // Возвращаем контент кнопки на "Проверить"
+            CheckBT.Content = "Проверить";
+        }
+
+
+        private void SetImagePosition(Image image, Point position)
+        {
+            Grid.SetColumn(image, (int)position.X);
+            Grid.SetRow(image, (int)position.Y);
         }
     }
 }
