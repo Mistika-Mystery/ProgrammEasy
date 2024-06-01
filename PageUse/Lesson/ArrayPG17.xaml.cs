da8be94072c79c724772015a2c2904ca53ede206
Мирослава Сергеева <m-bazhenova@yandex.ru>
2024-06-01 19:13:10 +0300

    zdfb

--- PageUse/Lesson/ArrayPG17.xaml.cs
+++ PageUse/Lesson/ArrayPG17.xaml.cs
@@ -24,5 +24,121 @@ namespace ProgrammEasy.PageUse.Lesson
         {
             InitializeComponent();
         }
+
+        private void BakcBT_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ArrayPG16());
+        }
+
+        private void NextBT_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ArrayPG18());
+        }
+
+        private void CheckBT_Click(object sender, RoutedEventArgs e)
+        {
+            if (CheckBT.Content.ToString() == "Проверить")
+            {
+                bool allCorrect = true;
+                int correctAnswers = 0;
+                int notCorrectAnswers = 0;
+
+                if (Check1.IsChecked == true)
+                {
+                    chNo1.Visibility = Visibility.Visible;
+                    allCorrect = false;
+                    notCorrectAnswers++;
+                }
+
+                if (Check2.IsChecked == true)
+                {
+                    chOk2.Visibility = Visibility.Visible;
+                    correctAnswers++;
+                }
+                else
+                {
+                    allCorrect = false;
+                }
+
+                if (Check3.IsChecked == true)
+                {
+                    chOk3.Visibility = Visibility.Visible;
+                    correctAnswers++;
+                }
+                else
+                {
+                    allCorrect = false;
+                }
+                if (Check5.IsChecked == true)
+                {
+                    chOk5.Visibility = Visibility.Visible;
+                    correctAnswers++;
+                }
+                else
+                {
+                    allCorrect = false;
+                }
+                if (Check4.IsChecked == true)
+                {
+                    chNo4.Visibility = Visibility.Visible;
+                    allCorrect = false;
+                    notCorrectAnswers++;
+                }
+                if (Check6.IsChecked == true)
+                {
+                    chNo6.Visibility = Visibility.Visible;
+                    allCorrect = false;
+                    notCorrectAnswers++;
+                }
+
+                if (correctAnswers == 0 && notCorrectAnswers == 0)
+                {
+                    HelpLB.Content = "Выберите варианты ответа";
+                    CheckBT.Content = "Попробовать еще раз";
+                }
+                else if (correctAnswers == 3 && allCorrect)
+                {
+                    NextBT.IsEnabled = true;
+                    CheckBT.Content = "Молодец!";
+                    HelpLB.Content = "";
+                }
+                else if (correctAnswers > 0 && notCorrectAnswers == 0)
+                {
+                    CheckBT.Content = "Попробовать еще раз";
+                    HelpLB.Content = "Нужно отметить ВСЕ верные значения";
+                }
+                else
+                {
+                    CheckBT.Content = "Попробовать еще раз";
+                    HelpLB.Content = "";
+                }
+            }
+            else if (CheckBT.Content.ToString() == "Молодец!")
+            { }
+            else
+            {
+                chNo1.Visibility = Visibility.Hidden;
+                chNo2.Visibility = Visibility.Hidden;
+                chNo3.Visibility = Visibility.Hidden;
+                chOk1.Visibility = Visibility.Hidden;
+                chOk2.Visibility = Visibility.Hidden;
+                chOk3.Visibility = Visibility.Hidden;
+                chNo4.Visibility = Visibility.Hidden;
+                chNo5.Visibility = Visibility.Hidden;
+                chNo6.Visibility = Visibility.Hidden;
+                chOk4.Visibility = Visibility.Hidden;
+                chOk5.Visibility = Visibility.Hidden;
+                chOk6.Visibility = Visibility.Hidden;
+
+                CheckBT.Content = "Проверить";
+                Check1.IsChecked = false;
+                Check2.IsChecked = false;
+                Check3.IsChecked = false;
+                Check4.IsChecked = false;
+                Check5.IsChecked = false;
+                Check6.IsChecked = false;
+                HelpLB.Content = "";
+            }
+        }
     }
 }
