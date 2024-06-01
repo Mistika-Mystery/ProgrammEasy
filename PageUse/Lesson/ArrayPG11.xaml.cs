2d178ff11313050c1b26cb7a949c708facc51852
Мирослава Сергеева <m-bazhenova@yandex.ru>
2024-06-01 12:26:37 +0300

    фаыв

--- PageUse/Lesson/ArrayPG11.xaml.cs
+++ PageUse/Lesson/ArrayPG11.xaml.cs
@@ -24,5 +24,95 @@ namespace ProgrammEasy.PageUse.Lesson
         {
             InitializeComponent();
         }
+
+        private void BakcBT_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ArrayPG10());
+        }
+
+        private void NextBT_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CheckBT_Click(object sender, RoutedEventArgs e)
+        {
+            if (CheckBT.Content.ToString() == "Проверить")
+            {
+                bool allCorrect = true;
+                int correctAnswers = 0;
+                int notCorrectAnswers = 0;
+
+                if (Check1.IsChecked == true)
+                {
+                    chNo1.Visibility = Visibility.Visible;
+                    allCorrect = false;
+                    notCorrectAnswers++;
+                }
+
+                if (Check2.IsChecked == true)
+                {
+                    chNo2.Visibility = Visibility.Visible;
+                    allCorrect = false;
+                    notCorrectAnswers++;
+                }
+
+                if (Check3.IsChecked == true)
+                {
+                    chOk3.Visibility = Visibility.Visible;
+                    correctAnswers++;
+                }
+                else
+                {
+                    allCorrect = false;
+                }
+
+                if (correctAnswers == 0 && notCorrectAnswers == 0)
+                {
+                    HelpLB.Content = "Выберите варианты ответа";
+                    CheckBT.Content = "Попробовать еще раз";
+                }
+                else if (correctAnswers == 1 && allCorrect && notCorrectAnswers == 0)
+                {
+                    NextBT.IsEnabled = true;
+                    CheckBT.Content = "Молодец!";
+                    HelpLB.Content = "";
+                }
+                else if (correctAnswers > 0 && notCorrectAnswers > 0)
+                {
+                    CheckBT.Content = "Попробовать еще раз";
+                    HelpLB.Content = "Выберите только один вариант";
+                    chNo1.Visibility = Visibility.Hidden;
+                    chNo2.Visibility = Visibility.Hidden;
+                    chNo3.Visibility = Visibility.Hidden;
+                    chOk1.Visibility = Visibility.Hidden;
+                    chOk2.Visibility = Visibility.Hidden;
+                    chOk3.Visibility = Visibility.Hidden;
+                }
+                else
+                {
+                    CheckBT.Content = "Попробовать еще раз";
+                    HelpLB.Content = "";
+                }
+            }
+            else if (CheckBT.Content.ToString() == "Молодец!")
+            { }
+            else
+            {
+                chNo1.Visibility = Visibility.Hidden;
+                chNo2.Visibility = Visibility.Hidden;
+                chNo3.Visibility = Visibility.Hidden;
+                chOk1.Visibility = Visibility.Hidden;
+                chOk2.Visibility = Visibility.Hidden;
+                chOk3.Visibility = Visibility.Hidden;
+
+                CheckBT.Content = "Проверить";
+                Check1.IsChecked = false;
+                Check2.IsChecked = false;
+                Check3.IsChecked = false;
+                HelpLB.Content = "";
+            }
+        }
     }
+    
 }
