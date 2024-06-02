1bc5367d86329a39c62e22c71a846c177a335769
Мирослава Сергеева <m-bazhenova@yandex.ru>
2024-06-02 09:54:00 +0300

    яч

--- PageUse/Lesson/TestEasyPG9.xaml.cs
+++ PageUse/Lesson/TestEasyPG9.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProgrammEasy.WinUse.Student;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace ProgrammEasy.PageUse.Lesson
 {
@@ -20,9 +22,82 @@ namespace ProgrammEasy.PageUse.Lesson
     /// </summary>
     public partial class TestEasyPG9 : Page
     {
-        public TestEasyPG9()
+        private DispatcherTimer _timer;
+        private DateTime _startTime;
+        private TestResult _testResult;
+        private int _questionNumber;
+        public TestEasyPG9(TestResult testResult, int questionNumber)
         {
             InitializeComponent();
+            _testResult = testResult;
+            _questionNumber = questionNumber;
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.Tick += Timer_Tick;
+            _startTime = DateTime.Now;
+            _timer.Start();
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            var elapsedTime = DateTime.Now - _startTime;
+        }
+        private void NextBT_Click(object sender, RoutedEventArgs e)
+        {
+            SaveAnswer();
+            _testResult.TotalTimeSpent += DateTime.Now - _startTime;
+            if (_questionNumber == _testResult.TotalQuestions)
+            {
+                _timer.Stop();
+                NavigationService.Navigate(new ResultPage(_testResult));
+            }
+            else
+            {
+                NavigationService.Navigate(new TestEasyPG9(_testResult, _questionNumber + 1)); // Переход к следующей странице
+            }
+        }
+
+        private void BakcBT_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show($"Вы уверены, что хотите вернуться?\nНесохраненные данные могут быть утеряны", "Внимание", MessageBoxButton.YesNo, MessageBoxImage.Exclamation) == MessageBoxResult.Yes)
+            {
+                _timer.Stop();
+                _testResult.QuestionResults.Clear();
+                try
+                {
+                    var logIn = new UserGlav();
+                    logIn.Show();
+
+                    Window window = Window.GetWindow(this);
+                    if (window != null)
+                    {
+                        window.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+        private void SaveAnswer()
+        {
+            var selectedAnswer = GetSelectedAnswer();
+            var correctAnswer = "a) array[index1, index2] = value;"; // Правильный ответ для текущего вопроса
+            _testResult.QuestionResults.Add(new QuestionResult
+            {
+                Question = QuestionBody.Text, // Текущий вопрос
+                SelectedAnswer = selectedAnswer, // Ответ пользователя
+                IsCorrect = selectedAnswer == correctAnswer // Проверка правильности ответа
+            });
+        }
+
+        private string GetSelectedAnswer()
+        {
+            if (AnswerRadioButton1.IsChecked == true) return "a) array[index1, index2] = value;";
+            if (AnswerRadioButton2.IsChecked == true) return "b) array.index1.index2 = value;";
+            if (AnswerRadioButton3.IsChecked == true) return "c) array(index1, index2) = value;";
+            if (AnswerRadioButton4.IsChecked == true) return "d) array{index1, index2} = value;";
+            return string.Empty;
         }
     }
 }
